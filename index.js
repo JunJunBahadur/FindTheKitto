47002d2e18af2252f44c1ef45558d5206fc74f2b
Arjun Senthil <arjunsenthil.dev@gmail.com>
2023-06-07 10:43:05 +0530

    added audio etc

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let catX, catY = 0;
 generateCat();
 
+setInterval(movement, 600);
 
-window.addEventListener('mousemove', mouseCoordinates);
 
+function movement() {
+    window.addEventListener('mousemove', mouseCoordinates);
+}
 
 function generateCat() {
-    document.getElementById('cat').src='cat.jpg';
+    //document.getElementById('cat').style.visibility = 'hidden';
+    
+    document.getElementById('catImg').src='cat.jpg';
     let x = Math.floor((Math.random() * screen.width) + 1);
     let y = Math.floor((Math.random() * screen.height) + 1);
     
@@ -17,11 +22,17 @@ function generateCat() {
         x = x-200;
     }
     
-    document.getElementById('cat').style.paddingLeft = x+'px';
-    document.getElementById('cat').style.paddingTop = y+'px';
+    document.getElementById('cat').style.marginLeft = x+'px';
+    document.getElementById('cat').style.marginTop = y+'px';
     
     catX = x+50;
     catY = y+50;
+
+}
+
+function found(){
+    document.getElementById('cat').style.visibility = "visible";
+    console.log('hello');
 }
 
 function mouseCoordinates(event){
@@ -31,11 +42,23 @@ function mouseCoordinates(event){
     let b = catY - event.clientY;
 
     let distance = Math.sqrt( a*a + b*b );
+    window.removeEventListener('mousemove', mouseCoordinates);
     console.log(distance);
-    //playAudio(distance);
+    playAudio(distance);
 }
 
 function playAudio(distance) {
-    var audio = new Audio('meow.mp3');
+    var audio = new Audio('cat.mp3');
+    if (distance>1000) {
+        audio.volume = 0.1;
+    } else if (distance>700 && distance<1000){
+        audio.volume = 0.2;
+    } else if (distance>400 && distance<700){
+        audio.volume = 0.4;
+    } else if (distance>100 && distance<400){
+        audio.volume = 0.7;
+    } else {
+        audio.volume = 1;
+    }
     audio.play();
 }
