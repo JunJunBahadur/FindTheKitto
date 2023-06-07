c51213a9358a85f388fffdb008640ed6a8e4cd09
Arjun Senthil <arjunsenthil.dev@gmail.com>
2023-06-07 12:03:24 +0530

    added score

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 let catX, catY = 0;
-generateCat();
+let distance = 1200;
+let won = false;
+let intervalId;
+localStorage.setItem("score", 0);
 
-setInterval(movement, 600);
+function gameEnd() {
+    document.getElementById('desc').style.visibility = 'visible';
+    document.getElementById('desc').innerHTML='You found the cat!';
+    document.getElementById('play').style.visibility = 'visible';
+    document.getElementById('play').innerHTML = 'Play again';
+}
 
+function gameStart() {
+    document.getElementById('cat').style.pointerEvents = 'auto';
+    document.getElementById('desc').style.visibility = 'hidden';
+    document.getElementById('play').style.visibility = 'hidden';
+    document.getElementById('cat').style.visibility = 'visible';
+    generateCat();
+    intervalId = setInterval(movement, 400);
+}
+
+function setScore() {
+    document.getElementById('score').innerHTML='score:'+localStorage.getItem('score');
+}
 
 function movement() {
     window.addEventListener('mousemove', mouseCoordinates);
 }
 
 function generateCat() {
-    //document.getElementById('cat').style.visibility = 'hidden';
+    document.getElementById('catImg').style.visibility = 'hidden';
     
     document.getElementById('catImg').src='cat.jpg';
     let x = Math.floor((Math.random() * screen.width) + 1);
@@ -27,38 +47,50 @@ function generateCat() {
     
     catX = x+50;
     catY = y+50;
-
+    
 }
 
 function found(){
-    document.getElementById('cat').style.visibility = "visible";
+    clearInterval(intervalId);
+    document.getElementById("cat").style.pointerEvents = "none";
+    document.getElementById('catImg').style.visibility = "visible";
+    let score = Number(localStorage.getItem("score"));
+    score = score+1;
+    localStorage.setItem("score", score);
+    setScore();
     console.log('hello');
+    gameEnd();
 }
 
 function mouseCoordinates(event){
     document.getElementById('axis').innerHTML='X:'+event.clientX+' Y:'+event.clientY;
-
+    
     let a = catX - event.clientX;
     let b = catY - event.clientY;
-
-    let distance = Math.sqrt( a*a + b*b );
+    
+    distance = Math.sqrt( a*a + b*b );
     window.removeEventListener('mousemove', mouseCoordinates);
-    console.log(distance);
     playAudio(distance);
+    console.log(distance);
 }
 
 function playAudio(distance) {
     var audio = new Audio('cat.mp3');
+    var audioFast = new Audio('cat(1).mp3');
+    
+    if (distance<200) {
+        audioFast.play();
+        return;
+    }
+
     if (distance>1000) {
         audio.volume = 0.1;
     } else if (distance>700 && distance<1000){
         audio.volume = 0.2;
     } else if (distance>400 && distance<700){
         audio.volume = 0.4;
-    } else if (distance>100 && distance<400){
-        audio.volume = 0.7;
     } else {
-        audio.volume = 1;
-    }
+        audio.volume = 0.7;
+    } 
     audio.play();
 }
